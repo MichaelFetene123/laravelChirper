a9618aca0dc6d8442dd6d9248576f3b618895de7
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-26 03:52:45 -0700

    Add card component and update ninja listing
    Introduced a reusable card Blade component for displaying ninja details. Updated the ninjas index view to use the new card component and highlight skilled ninjas. Adjusted ninja skill data in routes/web.php for demonstration.

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/ninjas', function (){
 
     $ninjas = [
         ['name' => 'miki', 'skill' => 75, 'id' => 1],
-        ['name' => 'emaye', 'skill' => 85, 'id' => 2]
+        ['name' => 'emaye', 'skill' => 47, 'id' => 2]
     ]; 
 
     return view('ninjas.index', ['greeting' => 'Hello', 'ninjas' => $ninjas]);
