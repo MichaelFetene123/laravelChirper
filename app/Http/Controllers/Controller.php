a80828dc4bcd89c4c714ea807e22d70b903d61f7
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-27 06:12:07 -0700

    Add NinjaController and seed Ninja data
    Introduces NinjaController and outlines RESTful methods in the base Controller. Updates DatabaseSeeder to include NinjaSeeder, which now seeds 50 Ninja records using a factory.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,32 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index(){
+        // ninjas -> /ninjas/
+        // fetch all ninjas from the database
+    }
+
+    public function show($id){
+        // ninjas -> /ninjas/1
+        // fetch a single ninja from the database by its id
+    }
+
+    public function create(){
+        // ninjas -> /ninjas/create
+        // render a create view (with web form) to user
+    }
+
+    public function store(){
+        // ninjas -> POST /ninjas
+        // handle the form submission from the create view
+        // validate input data
+        // save new ninja to the database
+    }
+
+  public function destroy($id){
+        // ninjas -> DELETE /ninjas/1
+        // delete a ninja from the database by its id
+    }
+
+
 }
