578fb352158e250c1e3d3bcf750b2fff3db86b4a
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-27 08:03:25 -0700

    Move ninja resource logic to NinjaController
    Transferred index, show, and create methods from the base Controller to NinjaController for better separation of concerns. Updated routes to use NinjaController methods directly and enhanced the ninja show view to display detailed ninja information.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,36 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Ninja;
 
 abstract class Controller
 {
-    public function index(){
-        $ninjas = Ninja::orderBy('created_at', 'desc')->get();
-        return view('ninjas.index', ['ninjas' => $ninjas]);
-    }
-
-    public function show($id){
-        // ninjas -> /ninjas/1
-        // fetch a single ninja from the database by its id
-    }
-
-    public function create(){
-        // ninjas -> /ninjas/create
-        // render a create view (with web form) to user
-    }
-
-    public function store(){
-        // ninjas -> POST /ninjas
-        // handle the form submission from the create view
-        // validate input data
-        // save new ninja to the database
-    }
-
-  public function destroy($id){
-        // ninjas -> DELETE /ninjas/1
-        // delete a ninja from the database by its id
-    }
-
+   
 
 }
--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ninja;
 use Illuminate\Http\Request;
 
 class NinjaController extends Controller
 {
-    //
+   public function index(){
+        $ninjas = Ninja::orderBy('created_at', 'desc')->get();
+        return view('ninjas.index', ['ninjas' => $ninjas]);
+    }
+
+    public function show($id){
+        // ninjas -> /ninjas/1
+        $ninja = Ninja::findOrFail($id);
+
+     return view('ninjas.show', ["ninja" => $ninja]);
+    }
+
+    public function create(){
+        // ninjas -> /ninjas/create
+        return view('ninjas.create');
+    }
+
+    public function store(){
+        // ninjas -> POST /ninjas
+        // handle the form submission from the create view
+        // validate input data
+        // save new ninja to the database
+    }
+
+  public function destroy($id){
+        // ninjas -> DELETE /ninjas/1
+        // delete a ninja from the database by its id
+    }
+
 }
